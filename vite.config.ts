4f043d04fa8d57d1b5f805fd4d2044ecb03b4cd7
a1247418 <pflaenzchen@hotmail.com>
2025-06-15 18:16:22 +0200

    Fixing base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/a1247418.github.io/' : "/",
+  base: '/a1247418.github.io/',
   server: {
     host: "::",
     port: 8080,
